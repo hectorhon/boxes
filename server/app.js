5d0af1be86f6e088c8bf703b5ed5e9919564f449
hectorhon <hon.lianhung@gmail.com>
2020-05-04 21:11:22 +0800

    React hello world

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const contactsRouter = require('./contacts/routes')
+const widgetsRouter = require('./widgets/routes')
 
 const app = express()
 const port = 3000
@@ -17,9 +18,14 @@ app.set('view options', {
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  res.render('home', {
+    title: 'Home',
+  })
+})
 
 app.use(contactsRouter)
+app.use(widgetsRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
