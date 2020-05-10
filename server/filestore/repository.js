8e637be0a45673ec65df7bd284470b3e5a424a14
hectorhon <hon.lianhung@gmail.com>
2020-05-10 16:26:11 +0800

    Get images via API

--- server/filestore/repository.js
+++ server/filestore/repository.js
@@ -1,13 +1,13 @@
 const db = require('../db')
 
 /*
-create table boxes_filestore (
-id serial primary key,
-name varchar,
-path varchar,
-add_date timestamp with time zone,
-mime_type varchar
-)
+  create table boxes_filestore (
+  id serial primary key,
+  name varchar,
+  path varchar,
+  add_date timestamp with time zone,
+  mime_type varchar
+  )
 */
 
 async function list() {
@@ -36,8 +36,33 @@ async function listImages() {
   return result.rows
 }
 
+async function searchImages(searchText = '', pageSize = 10, pageNumber = 1) {
+  const query = 'select id, name, path, add_date from boxes_filestore ' +
+    "where mime_type like 'image/%' " +
+    'and (name ilike $1) ' +
+    'limit $2 offset $3'
+  const result = await db.query(
+    query,
+    [`%${searchText}%`, pageSize, pageSize * (pageNumber - 1)]
+  )
+  return result.rows
+}
+
+async function getImageCount(searchText) {
+  const query = 'select count(*) as count from boxes_filestore ' +
+    "where mime_type like 'image/%' " +
+    'and (name ilike $1) '
+  const result = await db.query(
+    query,
+    [`%${searchText}%`]
+  )
+  return result.rows[0].count
+}
+
 module.exports = {
   list,
   insert,
   listImages,
+  searchImages,
+  getImageCount,
 }
