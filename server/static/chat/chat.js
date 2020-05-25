fecbf861a91470f93864d672028a14d465cf3b80
hectorhon <hon.lianhung@gmail.com>
2020-05-25 20:46:21 +0800

    socket.io

---
+++ server/static/chat/chat.js
@@ -0,0 +1,20 @@
+(function () {
+  // addEventListener so that duplicate tab works
+  document.addEventListener("DOMContentLoaded", function() {
+    const socket = io()
+    const form = document.querySelector('#chat-form')
+    const input = document.querySelector('#chat-form input')
+    const messageArea = document.querySelector('#message-area')
+    form.addEventListener('submit', function(e) {
+      e.preventDefault()
+      socket.emit('chat message', input.value)
+      input.value = ''
+      return false
+    })
+    socket.on('chat message', function (msg) {
+      const li = document.createElement('li')
+      li.innerText = msg
+      messageArea.append(li)
+    })
+  })
+})()
